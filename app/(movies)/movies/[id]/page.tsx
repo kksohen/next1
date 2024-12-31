200b7ede6d35aa31f444f161ce330af82280a1bd
kksohen <sh206304@gmail.com>
2024-12-31 17:43:03 +0900

    third add

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,24 @@
 //[]는 다이나믹 라우팅ㅇ
 //props는 params, searchParams를 가짐ㅇ
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-/* export default async function MovieDetail({params: {id}}: {params: {id: string}}) { */
-export default async function MovieDetail({params}: {params: Promise<{id: string}>}) {
-  const {id} = await params;
+interface IParams {
+  params: {
+    id: string;
+  };
+};
+
+/* 다이나믹 라우팅에 맞추어야 하는 동적인 타이틀 가능하게 해주는 함수 */
+export async function generateMetadata({params: {id}}: IParams) {
+  const movie = await getMovie(id);
+  return {
+  title: movie.title,
+  };
+};
+
+export default async function MovieDetail({params: {id}}: IParams) {
     /* 병렬적으로 데이터 패치할 것ㅇ 
     const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
     */
